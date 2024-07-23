6a4ba178f439d3cdf1b83d092f60dffcddd709ae
Jaime Gómez Moraleda <jaime@Jaimes-MacBook-Pro-2.local>
2024-07-23 22:25:28 +0200

    ecma script modules

---
+++ js-ts-njs/data-structures/modules/ecma-script/ecma-script-js-npm/main.js
@@ -0,0 +1,9 @@
+// main.js
+import { mod1Function } from './mod1.js'
+
+const testFunction = () => {
+    console.log('Im the main function')
+    mod1Function()
+}
+
+testFunction()
---
+++ js-ts-njs/data-structures/modules/ecma-script/ecma-script-js-npm/mod1.js
@@ -0,0 +1,3 @@
+// mod1.js
+const mod1Function = () => console.log('Mod1 is alive!')
+export { mod1Function }
